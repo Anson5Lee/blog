4af9dd6782ff6a0347fca78c349224feba9792f8
FRANK <frank5li@163.com>
2016-11-08 19:23:43 +0800

    add authenticate middleware

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-var express = require('express'),
-  routes = require('./routes'),
-  http = require('http'),
-  path = require('path'),
-  mongoskin = require('mongoskin'),
-  dbUrl = process.env.MONGOHQ_URL || 'mongodb://@localhost:27017/blog',
-  db = mongoskin.db(dbUrl, {safe: true}),
-  collections = {
-    articles: db.collection('articles'),
-    users: db.collection('users')
-  };
+// //////////////////////////////////////////////
+// Required
+// //////////////////////////////////////////////
+const http = require('http');
+const path = require('path');
 
-var session = require('express-session'),
-  logger = require('morgan'),
-  errorHandler = require('errorhandler'),
-  cookieParser = require('cookie-parser'),
-  bodyParser = require('body-parser'),
-  methodOverride = require('method-override');
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoskin = require('mongoskin');
+const logger = require('morgan');
+// A favicon is a visual cue that client software, like browsers, use to identify a site
+const favicon = require('serve-favicon');
+
+const  cookieParser = require('cookie-parser');
+const  session = require('express-session');
+
+const  bodyParser = require('body-parser');
+
+const routes = require('./routes');
+
+const dbUrl = process.env.MONGOHQ_URL || 'mongodb://@localhost:27017/blog';
+const db = mongoskin.db(dbUrl, {safe: true});
+const collections = {
+  articles: db.collection('articles'),
+  users: db.collection('users')
+};
+
+
+// Development-only error handler middleware.
+const  errorHandler = require('errorhandler');
+// Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
+const  methodOverride = require('method-override');
 
 var app = express();
+
 app.locals.appTitle = 'blog-express';
 
 app.use(function(req, res, next) {
@@ -26,16 +41,44 @@ app.use(function(req, res, next) {
   return next();
 });
 
+
+// app configurations 
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+
+// middleware configuration
+
 app.use(logger('dev'));
+app.use(methodOverride());
+app.use(favicon(__dirname + '/public/react.png'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(methodOverride());
+app.use(cookieParser());
+app.use(session({secret: "donothackmypersonalblog"}));
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Authentication middleware
+app.use(function(req, res, next) {
+  if (req.session && req.session.admin)
+    res.locals.admin = true;
+  next();
+});
+
+// Authorization Middleware
+var authorize = function(req, res, next) {
+  if (req.session && req.session.admin)
+    return next();
+  else
+    return res.send(401);
+};
+
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -44,19 +87,22 @@ if ('development' == app.get('env')) {
 
 // Pages and routes
 app.get('/', routes.index);
+
 app.get('/login', routes.user.login);
 app.post('/login', routes.user.authenticate);
 app.get('/logout', routes.user.logout);
+
 app.get('/admin',  routes.article.admin);
+
 app.get('/post',  routes.article.post);
 app.post('/post', routes.article.postArticle);
 app.get('/articles/:slug', routes.article.show);
 
 // REST API routes
-app.get('/api/articles', routes.article.list);
-app.post('/api/articles', routes.article.add);
-app.put('/api/articles/:id', routes.article.edit);
-app.del('/api/articles/:id', routes.article.del);
+// app.get('/api/articles', routes.article.list);
+// app.post('/api/articles', routes.article.add);
+// app.put('/api/articles/:id', routes.article.edit);
+// app.del('/api/articles/:id', routes.article.del);
 
 
 
@@ -85,3 +131,31 @@ if (require.main === module) {
   exports.shutdown = shutdown;
   exports.port = app.get('port');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-exports.article = require('./article');
-exports.user = require('./user');
+const article = require('./article');
+const user = require('./user');
 
 // get home page
 
-exports.index = function(req, res, next) {
+const index = function(req, res, next) {
 	req.collections.articles.find({published: true}, {sort: {_id:-1}}).toArray(function(error, articles){
     		if (error) return next(error);
     		res.render('index', { articles: articles});
 })}
+
+
+module.exports = {  
+	user: user,
+	article: article,
+	index: index
+}
